9c98dc5201809741b5c534342c0c2dc400f1a4ee
Tomio Ueda <tomio@jauntvr.com>
2016-02-17 14:15:47 -0800

    added max_pairwise

--- algorithmic-toolbox/1_week/max_pairwise_product.py
+++ algorithmic-toolbox/1_week/max_pairwise_product.py
@@ -1,13 +1,37 @@
-# Uses python3
-n = int(input())
-a = [int(x) for x in input().split()]
-assert(len(a) == n)
+"""
+max_pairwise_product.py
+By Tomio Ueda
 
-result = 0
+Problem
+Given a sequence of non-negative integers a0,…,an−1, 
+find the maximum pairwise product, that is, the largest 
+integer that can be obtained by multiplying two different 
+elements from the sequence.
 
-for i in range(0, n):
-    for j in range(i+1, n):
-        if a[i]*a[j] > result:
-            result = a[i]*a[j]
+Input format:
+The first line of the input contains an integer n. 
+The next line contains n non-negative integers a0,…,an−1 
+(separated by spaces).
 
-print(result)
+Constraints:
+2 <= n <= 2 * 105; 
+0≤a[0],…,a[n−1]≤10^5
+
+Output format:
+Output a single number — the maximum pairwise product.
+"""
+
+if __name__ == '__main__':
+
+    list_size = int(input())
+    input_numbers = [int(x) for x in input().split()]
+    assert(len(input_numbers) == list_size)
+
+    result = 0
+
+    for i in range(0, list_size):
+        for j in range(i+1, list_size):
+            if input_numbers[i]*input_numbers[j] > result:
+                result = input_numbers[i]*input_numbers[j]
+
+    print(result)
