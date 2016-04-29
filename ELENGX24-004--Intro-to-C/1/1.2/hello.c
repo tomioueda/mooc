d4c4b2226ae22192773645a2327171e465ddbac5
Tomio Ueda <tomio@jauntvr.com>
2016-04-29 15:47:04 -0700

    moved files around and added notes

---
+++ ELENGX24-004--Intro-to-C/1/1.2/hello.c
@@ -0,0 +1,20 @@
+//************************************************
+// Filename:   hello.c
+// Programmer: Tomio Ueda
+// Date:  April 29, 2016
+//
+// Description: 
+// Displays a hello message on screen to user. 
+// 
+// Build:
+// $ gcc -o hello hello.c
+//************************************************
+
+// preprocessor directive
+#include <stdio.h>
+
+int main(void) 
+{
+   printf("Hello"); // display  hello on screen
+   return 0;   // indicates success to OS
+}
--- ELENGX24-004--Intro-to-C/hello_world.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-    int num;
-    printf("Hello world! Give me an integer:\n");
-    scanf("%d", &num);
-    printf("Thanks! I've always been fond of %d.\n", num);
-    return 0;
-}
